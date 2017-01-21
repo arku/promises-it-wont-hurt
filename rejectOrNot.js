db120b7f12a6f76d38352a15c9ce742fc6b042ac
Arun Kumar <arunkumarm1995@gmail.com>
2017-01-21 07:34:31 +0530

    Complete reject-or-not challenge

---
+++ rejectOrNot.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const promise = new Promise((fulfill, reject) => {
+  fulfill('I FIRED');
+  reject(new Error('I DID NOT FIRE'));
+});
+
+const onReject = (err) => console.log(err.message);
+
+promise
+  .then(console.log, onReject);
